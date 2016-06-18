3fb32a73536b052aed1b6ea5a6325de69a7edf1e
Daniel Ramos <danielramosacosta@hotmail.com>
2016-06-18 05:03:12 +0100

    ; que sobran

--- src/scripts/components/layout/Nav.js
+++ src/scripts/components/layout/Nav.js
@@ -7,10 +7,10 @@ import '../../../styles/Nav'
 export default class Nav extends React.Component {
   render () {
     return (
-      <Navbar brand='Daniel Ramos' right>
-        <NavItem href='get-started.html'>Sobre mí</NavItem>
-        <NavItem href='components.html'>Carrera</NavItem>
-        <NavItem href='components.html'>Prácticas</NavItem>
+      <Navbar brand="Daniel Ramos" right>
+        <NavItem href="get-started.html">Sobre mí</NavItem>
+        <NavItem href="components.html">Carrera</NavItem>
+        <NavItem href="components.html">Prácticas</NavItem>
       </Navbar>
     )
   }
