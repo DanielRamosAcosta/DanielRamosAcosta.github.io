ac873e630b61960e5c5f276a8fe8423004b9084b
Daniel Ramos <danielramosacosta@hotmail.com>
2016-06-18 04:22:01 +0100

    Añadidas tareas al gulp

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const gulp = require('gulp')
+const spawn = require('child_process').spawn
+const del = require('del')
+
+gulp.task('build', ['clean'], (cb) => {
+  spawn('node', [
+    './node_modules/.bin/webpack',
+    '-c'
+  ], {stdio: 'inherit'})
+  .on('exit', (error) => {
+    if (error) {
+      console.log('Hubo errores!!!')
+    }
+    cb()
+  })
+})
+
+gulp.task('serve', ['clean'], (cb) => {
+  let ip = '0.0.0.0'
+
+  if (process.env.IP) {
+    ip = process.env.IP
+  }
+
+  spawn('node', [
+    './node_modules/.bin/webpack-dev-server',
+    '--content-base', 'src',
+    '--host', ip,
+    '--hot',
+    '--inline'
+  ], {stdio: 'inherit'})
+  .on('exit', (error) => {
+    if (error) {
+      console.log('Hubo errores!!!')
+    }
+    cb()
+  })
+})
+
+gulp.task('clean', () => {
+  return del([
+    'dist/**/*'
+  ])
+})
+
+gulp.task('deploy', ['build'], (cb) => {
+  console.log('Implementar el despliegue en gh-pages')
+  // TODO: Implementar esta tarea cuando tengamos algo más consisntente
+  cb()
+})
